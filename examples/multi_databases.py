d3976fd43a9e45be08a7702fce737d0dc06b89d0
dingyaguang <dingyaguang@itiger.com>
2019-12-25 12:05:49 +0800

    multiple

---
+++ examples/multi_databases.py
@@ -0,0 +1,91 @@
+from sqlalchemy import BigInteger, Column, String
+from tornado.gen import coroutine
+from tornado.ioloop import IOLoop
+from tornado.options import define, options, parse_command_line
+from tornado.web import Application, RequestHandler
+
+from tornado_sqlalchemy import (
+    SessionMixin,
+    as_future,
+    SQLAlchemy
+)
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = Column(BigInteger, primary_key=True)
+    username = Column(String(255))
+
+
+class Foo(db.Model):
+    __bind_key__ = 'foo'
+    __tablename__ = 'foo'
+
+    id = Column(BigInteger, primary_key=True)
+    foo = Column(String(255))
+
+
+
+class SynchronousRequestHandler(SessionMixin, RequestHandler):
+    def get(self):
+        with self.make_session() as session:
+            count = session.query(User).count()
+
+        # OR count = self.session.query(User).count()
+
+        self.write('{} users so far!'.format(count))
+
+
+class GenCoroutinesRequestHandler(SessionMixin, RequestHandler):
+    @coroutine
+    def get(self):
+        with self.make_session() as session:
+            count = yield as_future(session.query(User).count)
+
+        self.write('{} users so far!'.format(count))
+
+
+class NativeCoroutinesRequestHandler(SessionMixin, RequestHandler):
+    async def get(self):
+        with self.make_session() as session:
+            count = await as_future(session.query(User).count)
+
+        self.write('{} users so far!'.format(count))
+
+
+if __name__ == '__main__':
+
+    app = Application(
+        [
+            (r'/sync', SynchronousRequestHandler),
+            (r'/gen-coroutines', GenCoroutinesRequestHandler),
+            (r'/native-coroutines', NativeCoroutinesRequestHandler),
+        ],
+        SQLALCHEMY_DATABASE_URI='mysql://t_sa:t_sa@localhost/t_sa',
+        SQLALCHEMY_BINDS={
+            'foo': 'mysql://t_sa:t_sa@localhost/t_sa_1',
+            'bar': 'mysql://t_sa:t_sa@localhost/t_sa_2',
+        }
+    )
+    db.init_app(app)
+
+    db.create_all()
+
+    session = db.Session()
+    print(session)
+
+    session.add(User(username='a'))
+    session.commit()
+
+
+    # db.Model.metadata.create_all(db.get_engine())
+    # db.Model.metadata.create_all(db.get_engine('foo'))
+
+    print('Listening on port 8888')
+
+    app.listen(8888)
+    IOLoop.current().start()
---
+++ tests/test_engine_options.py
@@ -0,0 +1,73 @@
+from unittest import TestCase, mock
+
+from tornado.testing import AsyncTestCase, gen_test
+from tornado_sqlalchemy import as_future
+
+from ._common import db, User, mysql_url
+
+
+class EngineOptionTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.application = mock.Mock()
+        self.application.settings = {
+            'SQLALCHEMY_DATABASE_URI': mysql_url,
+            'SQLALCHEMY_ENGINE_OPTIONS': {
+                'echo': True,
+                'pool_size': 1,
+                'pool_timeout': 1
+            },
+        }
+        db.init_app(self.application)
+        db.create_all()
+
+    def tearDown(self) -> None:
+        db.drop_all()
+
+    def test_echo(self):
+        session = db.Session()
+        count = session.query(User).count()
+        print(count)
+        session.close()
+
+
+#
+# class ConcurrencyTestCase(AsyncTestCase):
+#     session_count = 5
+#     sleep_duration = 5
+#
+#     @gen_test
+#     def test_concurrent_requests_using_yield(self):
+#         factory = make_session_factory(postgres_url, pool_size=1)
+#
+#         sessions = [factory.make_session() for _ in range(self.session_count)]
+#
+#         yield [
+#             as_future(
+#                 lambda: session.execute(
+#                     'SELECT pg_sleep(:duration)',
+#                     {'duration': self.sleep_duration},
+#                 )
+#             )
+#             for session in sessions
+#         ]
+#
+#         for session in sessions:
+#             session.close()
+#
+#     @gen_test
+#     async def test_concurrent_requests_using_async(self):
+#         factory = make_session_factory(postgres_url, pool_size=1)
+#
+#         sessions = [factory.make_session() for _ in range(self.session_count)]
+#
+#         for session in sessions:
+#             await as_future(
+#                 lambda: session.execute(
+#                     'SELECT pg_sleep(:duration)',
+#                     {'duration': self.sleep_duration},
+#                 )
+#             )
+#
+#         for session in sessions:
+#             session.close()
